33d04a7bf7b0fdd56efc81936d3a4cd8c1f89dc7
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2025-02-13 12:15:47 +0600

    Create exam1.java

---
+++ exam5/exam1.java
@@ -0,0 +1,9 @@
+public class exam1 {
+
+  public static void main(String[] args) {
+    int[] array={1,2,4,2,1,5,6,9};
+    Innerexam obj=new Innerexam();
+    int sum=obj.sumArray(array);
+    System.out.println("sum= "+sum);
+  }
+}
