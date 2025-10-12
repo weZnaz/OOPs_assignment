b60cd1b38fd171fa0a10787e4f0d571be32bc240
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2025-10-12 20:58:21 +0600

    Create Music.java

---
+++ se1/Music.java
@@ -0,0 +1,23 @@
+package com;
+
+public class Music {
+    private String title;
+    private String artist;
+
+    public Music(String title, String artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void play() {
+        System.out.println("Playing: '" + title + "' by " + artist);
+    }
+}
