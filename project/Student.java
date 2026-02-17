ae1a95b90a3ae0368b35f0b47823d3b3e7876705
Syed Nazmul Islam <syednazmulislamramim@gmail.com>
2026-02-17 11:57:49 +0600

    Create Student.java

---
+++ project/Student.java
@@ -0,0 +1,18 @@
+package com.example.studentapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
